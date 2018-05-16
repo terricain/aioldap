219f5299ef2d5e1241165d60325aef7b3b8cae48
Terry Cain <TerryCain@fico.com>
2018-05-16 15:20:27 +0100

    Bump version: 0.2.0 → 0.2.1

--- aioldap/__init__.py
+++ aioldap/__init__.py
@@ -2,4 +2,4 @@ from .connection import LDAPConnection
 
 __all__ = ['LDAPConnection']
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='aioldap',
-    version='0.2.0',
+    version='0.2.1',
     description="Async ldap library sorta based off ldap3",
     long_description=readme + '\n\n' + history,
     author="Terry Cain",
